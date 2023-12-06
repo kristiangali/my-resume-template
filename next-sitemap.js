d46823c068fcd138c671ba538121384e15b2d8f0
kristiangali <kristiangali@protonmail.com>
2023-12-06 00:00:49 -0700

    fixed siteUrl

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  siteUrl: 'iamkg.com',
+  siteUrl: 'kristiangali.netlify.app',
   exclude: ['/404*', '/500*'],
   transform: async (config, path) => {
     return {
